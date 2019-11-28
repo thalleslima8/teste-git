1a3cb059af42a5482bade93375009601f59313b2
Thalles Veloso <thalleslima8@gmail.com>
2019-11-28 15:20:31 -0300

    Projeto iniciado, usuario entra com os dados de abertura de conta e entao faz o deposito inicial e depois o saque

---
+++ ContaBanco.cs
@@ -0,0 +1,59 @@
+﻿namespace ExercicioBanco {
+    class ContaBanco {
+
+        private string _nome;
+        public int Conta { get; private set; }
+        public int Agencia { get; private set; }
+        public double _saldo;
+
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null) {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Saldo {
+            get { return _saldo; }
+        }
+        public double Deposito {
+            set {
+                _saldo += value; 
+            }
+        }
+
+        public double Saque {
+            set {
+                _saldo -= value + 5.0;
+            }
+        }
+
+        public ContaBanco(string nome, int conta, int agencia) {
+            _nome = nome;
+            Conta = conta;
+            Agencia = agencia;
+            _saldo = 0;
+        }
+
+        public ContaBanco(string nome, int conta, int agencia, double saldo) : this(nome, conta, agencia) {
+            _nome = nome;
+            Conta = conta;
+            Agencia = agencia;
+            _saldo = saldo;
+        }
+
+
+        public override string ToString() {
+            return "Titular: " + _nome
+                    + ", Conta: " + Conta
+                    + ", Agencia: " + Agencia
+                    + ", Saldo: R$ " + _saldo.ToString("F2");
+        }
+
+
+    }
+        
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExercicioBanco {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Entre com o nome do Titular: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Entre com o numero da Conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o numero da Agencia: ");
+            int agencia = int.Parse(Console.ReadLine());
+            double saldo = 0;
+            Console.WriteLine("Haverá deposito inicial (s - sim ou n - não)");
+            string d = Console.ReadLine();
+
+            while (d.Contains('s') != true) {
+                if (d.Contains('n') == true) {
+                    break;
+                }
+                Console.WriteLine("Digite s - sim ou n - não");
+                d = Console.ReadLine();
+            }
+                       
+            if (d.Contains('s') == true || d.Contains('S') == true) {
+                Console.WriteLine("Entre com o deposito inicial: ");
+                saldo = double.Parse(Console.ReadLine());
+
+            }
+
+            ContaBanco c = new ContaBanco(nome, conta, agencia, saldo);
+            Console.WriteLine("Dados da Conta: " + c);
+
+            Console.WriteLine("Entre com um valor para Deposito: ");
+            c.Deposito = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados Atualizados:\n " + c);
+
+            Console.WriteLine("Entre com o valor do saque: ");
+            c.Saque = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados Atualizados:\n " + c);
+
+        }
+    }
+}
